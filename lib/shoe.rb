aa95f6c54380201171a5bfafdfd58c5f2624e08c
Annie Truex <artruex@gmail.com>
2019-05-31 03:32:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,15 +3,14 @@ def Shoe
   
   def initialize(shoe, brand)
     @shoe = shoe
+  end
+  
+  def brand=(brand)
     @brand = brand
   end
   
-#  def brand=(brand)
-#    @brand = brand
-#  end
-#  
-# def brand
-#   @brand
-#  end
+ def brand
+   @brand
+  end
   
 end
